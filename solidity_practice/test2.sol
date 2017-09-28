a5d0e7c2d986e0038c0d0d801cb45e5deedf9ec3
shibaken <kentaro.shibata@datasection.co.jp>
2017-09-28 17:33:11 +0900

    add and substract

---
+++ solidity_practice/test2.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.8;
+
+IntSample{
+    function division() constant returns (unit){
+        uint a=3;
+        uint b=2;
+        uint c=a/b*10;
+        return c;
+    }
+    function division Literals() constant returns(uint){
+        uint a=3;
+        uint c=a/0:
+    }
+    function shift() constant returns (uint[2]){
+
+        uint[2] a;
+        a[0]=16<<2;
+        a[1]=16>>2;
+
+
+    }
+
+
+}
